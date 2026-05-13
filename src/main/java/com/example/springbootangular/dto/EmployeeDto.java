56817cfc39f9142934b6b619fc588ae19f2bc5f8
Niharpradhan-8 <pradhannihar8@gmail.com>
2026-05-13 22:30:06 +0530

    dto validation added.

--- src/main/java/com/example/springbootangular/dto/EmployeeDto.java
+++ src/main/java/com/example/springbootangular/dto/EmployeeDto.java
@@ -1,11 +1,25 @@
 package com.example.springbootangular.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployeeDto {
+    @NotBlank(message = "Name should not be blank")
+    private String name;
+    @NotBlank(message = "Email should not be blank")
+    private String email;
+    @PastOrPresent(message = "Date of birth should be a past date")
+    private LocalDate dob;
+    @NotBlank(message = "address should not be blank")
+    private String address;
+    @NotBlank(message = "phone number should not be null")
+    private String phoneNumber;
 }
