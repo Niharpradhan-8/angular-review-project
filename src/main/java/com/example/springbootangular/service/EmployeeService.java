7bc45ed1b6cbe005a56cdfac17f7d7b8e3674f23
Niharpradhan-8 <pradhannihar8@gmail.com>
2026-05-14 21:57:43 +0530

    controller and service added.

--- src/main/java/com/example/springbootangular/service/EmployeeService.java
+++ src/main/java/com/example/springbootangular/service/EmployeeService.java
@@ -1,7 +1,11 @@
 package com.example.springbootangular.service;
 
+import com.example.springbootangular.dto.EmployeeDto;
+import com.example.springbootangular.entity.Employee;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
+
+    public Employee saveEmployee(EmployeeDto employeeDto);
 }
--- src/main/java/com/example/springbootangular/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootangular/service/EmployeeServiceImpl.java
@@ -1,4 +1,15 @@
 package com.example.springbootangular.service;
 
+import com.example.springbootangular.dto.EmployeeDto;
+import com.example.springbootangular.entity.Employee;
+
 public class EmployeeServiceImpl implements EmployeeService{
+    @Override
+    public Employee saveEmployee(EmployeeDto employeeDto) {
+        try {
+        } catch (Exception e) {
+
+        }
+        return null;
+    }
 }
