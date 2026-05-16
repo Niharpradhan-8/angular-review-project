b6427a93dac0eedb92c365a8d40196d171eca000
Niharpradhan-8 <pradhannihar8@gmail.com>
2026-05-16 23:23:06 +0530

    delete employee method added.

--- src/main/java/com/example/springbootangular/service/EmployeeService.java
+++ src/main/java/com/example/springbootangular/service/EmployeeService.java
@@ -2,10 +2,13 @@ package com.example.springbootangular.service;
 
 import com.example.springbootangular.dto.EmployeeDto;
 import com.example.springbootangular.entity.Employee;
+import com.example.springbootangular.entity.EmployeePk;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface EmployeeService {
 
     public Employee saveEmployee(EmployeeDto employeeDto);
+
+    public void deleteEmployee(EmployeePk id);
 }
--- src/main/java/com/example/springbootangular/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootangular/service/EmployeeServiceImpl.java
@@ -2,14 +2,35 @@ package com.example.springbootangular.service;
 
 import com.example.springbootangular.dto.EmployeeDto;
 import com.example.springbootangular.entity.Employee;
+import com.example.springbootangular.entity.EmployeePk;
+import com.example.springbootangular.exception.RecordNotFoundException;
+import com.example.springbootangular.repository.EmployeeRepository;
 
 public class EmployeeServiceImpl implements EmployeeService{
+
+     private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     @Override
     public Employee saveEmployee(EmployeeDto employeeDto) {
         try {
+
         } catch (Exception e) {
 
         }
         return null;
+    }
+
+    @Override
+    public void deleteEmployee(EmployeePk id) {
+        try{
+            Employee employee = employeeRepository.findById(id).orElseThrow(() ->new RecordNotFoundException("Employee not found"));
+            employeeRepository.deleteById(id);
+        }catch(Exception e){
+            throw new RecordNotFoundException(e.getMessage());
+        }
     }
 }
